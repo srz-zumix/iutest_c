7300cde4b4058b0d8e78da99cc2ec6ea53480554
srz-zumix <zumix.cpp@gmail.com>
2014-01-26 18:00:02 +0900

    update r465

--- include/iutest_ver_c.h
+++ include/iutest_ver_c.h
@@ -8,7 +8,7 @@
  * @version		1.0
  *
  * @par			copyright
- * Copyright (C) 2012-2013, Takazumi Shirayanagi\n
+ * Copyright (C) 2012-2014, Takazumi Shirayanagi\n
  * This software is released under the new BSD License,
  * see LICENSE
 */
@@ -30,7 +30,7 @@
  *			sourceforge : http://sourceforge.jp/projects/iutest/ \n
  *
  * @par		copyright
- * Copyright (C) 2012-2013, Takazumi Shirayanagi\n
+ * Copyright (C) 2012-2014, Takazumi Shirayanagi\n
  * This software is released under the new BSD License.
  *
  * @par		LICENSE
